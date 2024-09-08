22a072052462a91cae48eb111d1b3cab20afb716
dhrruvchotai <23010101051@darshan.ac.in>
2024-09-08 21:16:32 +0530

    updated

--- Lab-18/BinarySerch.java
+++ Lab-18/BinarySerch.java
@@ -1,19 +1,19 @@
-public class BinarySerch {
+public class BinarySerch{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         
-        int indx = binarySerch(arr, 0, arr.length-1, 2);
+        int indx = binarySerchSimple(arr, 0, arr.length-1, 2);
 
         if(indx == -1){
             System.out.println("Elmt not found!!");
         }
         else{
-            System.out.println("Elmt is at index :"+indx);
+            System.out.println("Elmt is at index : "+indx);
         }
 
     }
 
-    public static int binarySerch(int arr[],int low,int high,int elmt){
+    public static int binarySerchRecr(int arr[],int low,int high,int elmt){
 
         int mid = (low+high)/2;
 
@@ -22,11 +22,34 @@ public class BinarySerch {
         }
 
         else if(elmt < arr[mid]){
-            return binarySerch(arr,low,mid-1,elmt);
+            return binarySerchRecr(arr,low,mid-1,elmt);
         }
 
         else if(elmt > arr[mid]){
-           return binarySerch(arr,mid+1,high,elmt);
+           return binarySerchRecr(arr,mid+1,high,elmt);
+        }
+
+        return -1;
+
+    }
+
+    public static int binarySerchSimple(int arr[],int low,int high,int elmt){
+
+        
+        while(low <= high){
+            
+            int mid = (low+high)/2;
+
+            if(elmt == arr[mid]){
+                return mid;
+            }
+            else if(elmt < arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+
         }
 
         return -1;
--- Lab-19/BubbleSort.java
+++ Lab-19/BubbleSort.java
@@ -2,17 +2,18 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = new int[]{5,8,2,1,9};
        
-        for(int i=0;i<=(arr.length-2);i++){
-            for(int j=i+1;j<(arr.length-1);j++){
-                if(arr[i]>arr[j]){
+        for(int i=0;i<(arr.length-1);i++){
+            for(int j=0;j<(arr.length-1);j++){
+                if(arr[j]>arr[j+1]){
                     int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
-        System.out.println("the arr is");  for(int i=0;i<arr.length;i++){
-            
+        System.out.println("the arr is");  
+
+        for(int i=0;i<arr.length;i++){   
             System.out.println(arr[i]);
         }
     }
